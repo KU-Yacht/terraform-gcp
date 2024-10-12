682cf788a103b47fd0ecb449a58e7132c78fe435
KangManJoo <eogns47@konkuk.ac.kr>
2024-10-12 11:30:40 +0900

    feat: Open ingress controller port
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- worker/main.tf
+++ worker/main.tf
@@ -1,3 +1,29 @@
+resource "google_compute_firewall" "allow-4430-external" {
+  name    = "allow-external-4430"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["4430"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]  
+  target_tags   = ["kube-worker"] 
+}
+
+resource "google_compute_firewall" "allow-ingress-nodeports" {
+  name    = "allow-ingress-nodeports"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["31595", "32759"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["kube-worker"]
+}
+
 resource "google_compute_address" "static_ip" {
   count  = var.worker_count
   name   = "my-static-ip-${count.index + 1}"
@@ -23,4 +49,7 @@ resource "google_compute_instance" "default" {
         nat_ip = google_compute_address.static_ip[count.index].address
     }
   }
+
+  tags = ["kube-worker"]
+
 }
